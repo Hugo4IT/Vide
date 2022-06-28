9bc0d86fb516afbbc8f471cabdd54719275d1028
Hugo4IT <hugo.vandekuilen1234567890@gmail.com>
2022-06-28 17:03:03 +0200

    correction

--- examples/animation/src/main.rs
+++ examples/animation/src/main.rs
@@ -14,20 +14,20 @@ fn main() {
     root.new_clip(1.0..5.0)
         .effect(Rect {                                                  // Solid rectangle
             position: keyframes!(
-                initial (0.0, -150.0),                                  // Frame 0  = (0.0, -800.0)
-                30 => OUT_BACK => (0.0, 0.0),                           // Frame 15 = (0.0, 0.0) aka. Center
-                                                                        // Transition from frame 0 to 15 handled with EASE_OUT_BACK
+                initial (0.0, -150.0),                                  // Frame 0  = (0.0, -150.0)
+                30 => OUT_BACK => (0.0, 0.0),                           // Frame 30 = (0.0, 0.0) aka. Center
+                                                                        // Transition from frame 0 to 30 handled with EASE_OUT_BACK
             ),
             size: keyframes!(
-                initial (350.0, 250.0),                                 // Frame 0  = (300.0, 200.0)
-                30 => OUT_CUBIC => (400.0, 300.0),                      // Frame 15 = (400.0, 300.0)
-                                                                        // Transition from frame 0 to 15 handled with EASE_OUT_BACK
+                initial (350.0, 250.0),                                 // Frame 0  = (350.0, 250.0)
+                30 => OUT_CUBIC => (400.0, 300.0),                      // Frame 30 = (400.0, 300.0)
+                                                                        // Transition from frame 0 to 30 handled with EASE_OUT_CUBIC
             ),
             color: keyframes!(
                 initial rgba8!(0xda, 0x00, 0x37, 0x00),                 // Frame 0  = #da003700
-                30 => OUT_QUADRATIC => rgba8!(0xda, 0x00, 0x37, 0xFF),  // Frame 30 = #da0037FF (EASE_OUT_BACK from frame 0 to 15 - fade in)
-                45 => LINEAR => rgba8!(0xda, 0x00, 0x37, 0xFF),         // Frame 30 = #da0037FF (State holds for 30 frames)
-                60 => IN_QUADRATIC => rgba8!(0x00, 0xda, 0x37, 0xFF),   // Frame 45 = #00da37FF (EASE_IN_QUADRATIC from frame 30 to 45 - change color)
+                30 => OUT_QUADRATIC => rgba8!(0xda, 0x00, 0x37, 0xFF),  // Frame 30 = #da0037FF (EASE_OUT_QUADRATIC from frame 0 to 15 - fade in)
+                45 => LINEAR => rgba8!(0xda, 0x00, 0x37, 0xFF),         // Frame 45 = #da0037FF (State holds for 30 frames)
+                60 => IN_QUADRATIC => rgba8!(0x00, 0xda, 0x37, 0xFF),   // Frame 60 = #00da37FF (EASE_IN_QUADRATIC from frame 30 to 45 - change color)
             ),
         });
 
