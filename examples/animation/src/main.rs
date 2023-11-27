3fee6b5a283cebac90ad85ec25174460a1cf0337
Hugo4IT <hugo.vandekuilen1234567890@gmail.com>
2023-11-27 14:45:55 +0100

    asdf

--- examples/animation/src/main.rs
+++ examples/animation/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     });
 
     let root = video.root();
+
     root.new_clip(1.0..5.0).effect(Rect {
         position: unanimated!((-300.0, 0.0)),
         size: unanimated!((200.0, 150.0)),
@@ -22,6 +23,7 @@ fn main() {
             .keyframe(Rel(0.3), ease::IN_QUADRATIC,   "#00da37")
             .build(),
     });
+
     root.new_clip(1.0..5.0).effect(Rect {
         position: unanimated!((0.0, 0.0)),
         size: unanimated!((200.0, 150.0)),
@@ -32,6 +34,7 @@ fn main() {
             .keyframe(Rel(0.3), ease::IN_QUADRATIC,   "#00da37")
             .build(),
     });
+
     root.new_clip(1.0..5.0).effect(Rect {
         position: unanimated!((300.0, 0.0)),
         size: unanimated!((200.0, 150.0)),
@@ -41,6 +44,15 @@ fn main() {
             .hold(0.3)
             .keyframe(Rel(0.3), ease::IN_QUADRATIC,   "#00da37")
             .build(),
+    });
+
+    root.new_clip(0.0..5.0).effect(Rect {
+        position: unanimated!((0.0, 0.0)),
+        size: Animation::new(60.0)
+            .keyframe(Abs(0.0), ease::LINEAR,          (0.0, 1080.0))
+            .keyframe(Rel(0.9), ease::OUT_EXPONENTIAL, (1920.0, 1080.0))
+            .build(),
+        color: unanimated!("#0037da"),
     });
 
     video.render(vide::quick_export::to("output.mp4"));
